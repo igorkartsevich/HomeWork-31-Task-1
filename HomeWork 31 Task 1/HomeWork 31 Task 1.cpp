8aae2d2f901ed5f6f859bd410098f256a5cd576c
igor_kartsevich <71553499+igorkartsevich@users.noreply.github.com>
2022-02-13 08:33:47 +0300

    13/02/01

--- HomeWork 31 Task 1/HomeWork 31 Task 1.cpp
+++ HomeWork 31 Task 1/HomeWork 31 Task 1.cpp
@@ -15,43 +15,34 @@ class shared_ptr_toy {
 public:
     shared_ptr_toy() : shared_ptr_toy("SomeToy") {}
 
-    shared_ptr_toy(std::string inName_toy) {
-        ptr = new Toy(inName_toy);
-        ptr_counter = new int(1);
-    }
+    shared_ptr_toy(std::string inName_toy) : ptr(new Toy(inName_toy)), ptr_counter(new int(1)) {}
 
-    shared_ptr_toy(const Toy& other_toy) {
-        ptr = new Toy(other_toy);
-        ptr_counter = new int(1);
-    }
+    shared_ptr_toy(const Toy& other_toy) : ptr(new Toy(other_toy)), ptr_counter(new int(1)) {}
 
-    shared_ptr_toy(const shared_ptr_toy& other_ptr) : ptr(other_ptr.ptr) {
-        ptr_counter = new int(*other_ptr.ptr_counter + 1);
-    }
+    shared_ptr_toy(const shared_ptr_toy& other_ptr) : ptr(other_ptr.ptr), ptr_counter(new int(*other_ptr.ptr_counter + 1)) {}
 
     shared_ptr_toy& operator=(const shared_ptr_toy& other_ptr) {
         if (this == &other_ptr) return *this;
-        if (ptr != nullptr) {
-            if (--(*ptr_counter) == 0) {
-                delete ptr;
-                delete ptr_counter;
-            }
-        }
+        if (ptr != nullptr) delete_ref();
         ptr = other_ptr.ptr;
         ptr_counter = new int(*other_ptr.ptr_counter + 1);
         return *this;
     }
 
     ~shared_ptr_toy() {
-        if (--(*ptr_counter) == 0) {
-            delete ptr;
-            delete ptr_counter;
-        }
+        delete_ref();
     }
 
 private:
     Toy* ptr = nullptr;
     int* ptr_counter = nullptr;
+
+    void delete_ref() {
+        if (--(*ptr_counter) == 0) {
+            delete ptr; ptr = nullptr;
+            delete ptr_counter; ptr_counter = nullptr;
+        }
+    }
 };
 
 Toy make_shared_toy(std::string name) {
